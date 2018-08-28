71acdc3ef83ebe6c52a15bbb9d1128b492c088bd
ysjk2003 <ysjk2003@gmail.com>
2018-08-28 18:55:49 +0900

    temp

--- Hello_Flask.py
+++ Hello_Flask.py
@@ -1,11 +1,12 @@
 #-*- coding: utf-8 -*-
-
+#utf-8로 인코딩
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_flask():
+app = Flask(__name__)  #객체 생성
+
+@app.route('/')  #root에 라우팅
+def hello_flask():  #특정 URL을 호출했을 때 호출되는 함수 정의, 뷰 함수라고 함
     return 'Hello Flask!'
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == '__main__':  #실행되는 모듈이 파이썬 인터프리터에 의해 메인 모듈로 실행됐는지 임포트되어 사용됐는지 확인하여
+    app.run()               #메인 모듈로 실행됐으면 테스토 용도로 사용되는 로컬 서버인 run()함수를 실행한다.
